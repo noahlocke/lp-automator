66f55aa4672683ed9d61c3923d8ebf2122b73e08
Locke Noah J <nlocke@uwhealth.org>
2016-04-28 15:35:48 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var jsonParser = bodyParser.json();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -15,6 +18,13 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
+});
+
+app.post('/liquid-task', jsonParser, function (request, response) {
+	response.sendStatus(200);
+	if (request.body.is_estimated === false) {
+		console.log(request.body.name + " has to time estimate!");
+	}
 });
 
 
