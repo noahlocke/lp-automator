da9934195b219e6f00ba718ddea9c8beea32d312
Locke Noah J <nlocke@uwhealth.org>
2016-04-29 10:42:07 -0500

    logging data output

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ function addEstimate(taskId,assId) {
 		    "low_effort_remaining": 0.1,
 		    "high_effort_remaining": 1
 	};
+	console.log(JSON.stringify(data));
 	request(
 	    { 	method: 'POST', 
         		uri: url, 
