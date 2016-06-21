38b117d29a8b2adbdc2130fd80b604451e64466f
Locke Noah J <nlocke@uwhealth.org>
2016-06-21 17:09:28 -0500

    testing...

--- index.js
+++ index.js
@@ -32,10 +32,11 @@ app.listen(app.get('port'), function() {
 app.post('/liquid-task', jsonParser, function (request, response) {
 	response.sendStatus(200);
 	if (request.body.change_type === "create") {
-		if (request.body.is_estimated === false) {
-			console.log(request.body.id + " has no time estimate! Let's fix that...");
-			addEstimate(request.body.id,request.body.assignments[0].id);
-		} else if (request.body.name === "TEMPLATE: Bug Fix") {
+		// if (request.body.is_estimated === false) {
+		// 	console.log(request.body.id + " has no time estimate! Let's fix that...");
+		// 	addEstimate(request.body.id,request.body.assignments[0].id);
+		// } else 
+		if (request.body.name === "TEMPLATE: Bug Fix") {
 			console.log(request.body.id + " needs to be packaged, let me take care of that for you.");
 			packageMe(request.body.id);
 		}
